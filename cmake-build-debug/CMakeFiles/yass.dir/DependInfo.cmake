43e2c79d03b4b3357a8e38aa9c2278bdc3bed22f
Strideradu <dunan00001@hotmail.com>
2017-06-30 14:38:10 -0400

    create clion project lala

---
+++ cmake-build-debug/CMakeFiles/yass.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/align.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/align.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/assemble.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/assemble.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/avl.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/avl.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/display.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/display.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/global_var.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/global_var.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/kword.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/kword.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/list.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/list.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/main.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/main.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/prdyn.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/prdyn.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/proba.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/proba.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/red_black.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/red_black.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/regroup.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/regroup.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/threads.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/threads.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/tuple.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/tuple.c.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/yass/src/util.c" "/cygdrive/c/Users/Nan/Documents/GitHub/yass/cmake-build-debug/CMakeFiles/yass.dir/src/util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/yass.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yass.dir/src/align.c.o"
+  "CMakeFiles/yass.dir/src/assemble.c.o"
+  "CMakeFiles/yass.dir/src/avl.c.o"
+  "CMakeFiles/yass.dir/src/display.c.o"
+  "CMakeFiles/yass.dir/src/global_var.c.o"
+  "CMakeFiles/yass.dir/src/kword.c.o"
+  "CMakeFiles/yass.dir/src/list.c.o"
+  "CMakeFiles/yass.dir/src/main.c.o"
+  "CMakeFiles/yass.dir/src/prdyn.c.o"
+  "CMakeFiles/yass.dir/src/proba.c.o"
+  "CMakeFiles/yass.dir/src/red_black.c.o"
+  "CMakeFiles/yass.dir/src/regroup.c.o"
+  "CMakeFiles/yass.dir/src/threads.c.o"
+  "CMakeFiles/yass.dir/src/tuple.c.o"
+  "CMakeFiles/yass.dir/src/util.c.o"
+  "yass.pdb"
+  "yass.exe"
+  "libyass.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/yass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
