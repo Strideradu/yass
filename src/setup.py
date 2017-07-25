7865c439d6b78a43be361344b69515e97bc9667d
Nan Du <dunan00001@gmail.com>
2017-07-24 22:53:06 -0400

    for the prob test module

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup, Extension
 
 # define the extension module
-prob_module = Extension('prob_module', sources=['prob_module.c'])
+prob_module = Extension('prob_module',
+                        sources=['prob_module.c', 'proba.c', 'global_var.c', 'tuple.c', 'kword.c', 'util.c', 'prdyn.c'])
 
 # run the setup
 setup(ext_modules=[prob_module])
